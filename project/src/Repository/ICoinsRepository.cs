5f630a44c0f11842858945bc32d1bdee28f0cbd7
Tanasa Stefan-Alexandru <147146093+Jevan2004@users.noreply.github.com>
2025-04-07 22:57:35 +0300

    Some tests and refactor (#13)

---
+++ project/src/Repository/ICoinsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseApp.Repository
+{
+    public interface ICoinsRepository
+    {
+        void InitializeUserWalletIfNotExists(int userId, int initialCoinBalance = 0);
+        int GetUserCoinBalance(int userId);
+        void SetUserCoinBalance(int userId, int updatedCoinBalance);
+        DateTime GetUserLastLoginTime(int userId);
+        void UpdateUserLastLoginTimeToNow(int userId);
+        void AddCoinsToUserWallet(int userId, int amountToAdd);
+        bool TryDeductCoinsFromUserWallet(int userId, int deductionAmount);
+    }
+}
