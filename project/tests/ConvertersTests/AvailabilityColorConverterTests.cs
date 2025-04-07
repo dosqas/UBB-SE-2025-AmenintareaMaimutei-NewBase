6e6e0b6f060dd2c4e555bac35e49ba72e5d994d0
Iulia <147263035+iuliaszarics@users.noreply.github.com>
2025-04-07 15:36:29 +0300

    Availability color converter tests (#11)
    * Added test for AvailabilityColorConverter
    
    * Changed tests to Tests for Style reasons

--- project/tests/ConvertersTests/AvailabilityColorConverterTests.cs
+++ project/tests/ConvertersTests/AvailabilityColorConverterTests.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.UI.Xaml.Media;
-using Xunit;
-using Windows.UI;
-using CourseApp.Converters;
-using Microsoft.UI;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml;
-using Xunit;
-
-
-namespace CourseApp.Tests.Converters
+﻿namespace Tests.ConvertersTests
 {
+    using System;
+    using CourseApp.Converters;
+    using Microsoft.UI.Xaml.Media;
+    using Windows.UI;
+    using Xunit;
+
+    public class AvailabilityColorConverterTests
+    {
+        private readonly AvailabilityColorConverter testConverter;
+
+        public AvailabilityColorConverterTests()
+        {
+            this.testConverter = new AvailabilityColorConverter();
+        }
+
+        /// <summary>
+        /// Tests that the ConvertBack method throws a NotImplementedException.
+        /// </summary>
+        [Fact]
+        public void ConvertBackShouldThrowNotImplementedException()
+        {
+            // Arrange
+            var input = true;
+
+            // Act & Assert
+            var exception = Assert.Throws<NotImplementedException>(() =>
+                this.testConverter.ConvertBack(input, typeof(bool), null!, null!));
+            Assert.Equal("Reverse conversion is not supported", exception.Message);
+        }
+    }
 }
