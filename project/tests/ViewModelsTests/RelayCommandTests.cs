48fc675d8206cb35747841c2b2352222f59514ba
Dragoș Șerban <dragos4002@gmail.com>
2025-04-11 15:06:01 +0300

    Added unit tests for RelayCommand functionality

---
+++ project/tests/ViewModelsTests/RelayCommandTests.cs
@@ -0,0 +1,86 @@
+﻿namespace Tests.ViewModelsTests
+{
+    using System;
+    using System.Windows.Input;
+    using CourseApp.ViewModels;
+    using Xunit;
+
+    public class RelayCommandTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowArgumentNullException_WhenExecuteIsNull()
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new RelayCommand(null));
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenNoPredicateProvided()
+        {
+            // Arrange
+            var command = new RelayCommand(_ => { });
+
+            // Act
+            var result = command.CanExecute(null);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnPredicateResult_WhenPredicateProvided()
+        {
+            // Arrange
+            var command = new RelayCommand(_ => { }, _ => false);
+
+            // Act
+            var result = command.CanExecute(null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Execute_ShouldInvokeExecuteAction()
+        {
+            // Arrange
+            bool executed = false;
+            var command = new RelayCommand(_ => executed = true);
+
+            // Act
+            command.Execute(null);
+
+            // Assert
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void RaiseCanExecuteChanged_ShouldTriggerCanExecuteChangedEvent()
+        {
+            // Arrange
+            var command = new RelayCommand(_ => { });
+            bool eventRaised = false;
+
+            command.CanExecuteChanged += (_, _) => eventRaised = true;
+
+            // Act
+            command.RaiseCanExecuteChanged();
+
+            // Assert
+            Assert.True(eventRaised);
+        }
+
+        [Fact]
+        public void RaiseCanExecuteChanged_ShouldNotThrow_WhenNoSubscribers()
+        {
+            // Arrange
+            var command = new RelayCommand(_ => { });
+
+            // Act & Assert
+            var exception = Record.Exception(() => command.RaiseCanExecuteChanged());
+
+            Assert.Null(exception);
+        }
+
+    }
+}
