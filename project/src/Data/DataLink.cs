bcab85c3bb9e02d36429f86523a85d29c2611a6d
dosqas <137123562+dosqas@users.noreply.github.com>
2025-04-12 13:58:04 +0300

    Added summary to DataLink

--- project/src/Data/DataLink.cs
+++ project/src/Data/DataLink.cs
@@ -3,11 +3,21 @@ using Microsoft.Data.SqlClient;
 
 namespace CourseApp.Data
 {
+    /// <summary>
+    /// Provides a static method to retrieve a SQL connection to the CourseApp database.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     public class DataLink
     {
-		private static readonly string ConnectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=CourseApp;Integrated Security=True;TrustServerCertificate=True";
+        /// <summary>
+        /// The connection string used to connect to the CourseApp database.
+        /// </summary>
+        private static readonly string ConnectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=CourseApp;Integrated Security=True;TrustServerCertificate=True";
 
+        /// <summary>
+        /// Retrieves a new SQL connection using the predefined connection string.
+        /// </summary>
+        /// <returns>A new instance of <see cref="SqlConnection"/> connected to the CourseApp database.</returns>
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(ConnectionString);
